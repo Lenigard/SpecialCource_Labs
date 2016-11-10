62e17d9cd0b88659966b086d107ffaa6ccc56617
Lenigard <lenigard@mail.ru>
2016-11-10 12:24:58 +0300

    Updated exercise

--- 1_firstSteps/volume3cube.py
+++ 1_firstSteps/volume3cube.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
+from numpy import linspace 
+import matplotlib.pyplot as plt
 
-larray = np.linspace(1, 3, num = 3)
 
-v1 = larray[0]**3
-v2 = larray[1]**3
-v3 = larray[2]**3
-v = larray**3
+l = linspace(1, 3, num = 3)
+
+v1_correct = 1.
+v2_correct = 8.
+v3_correct = 27.
+v = l**3
 
 print "Вычисление объёма вручную (поэлементно):"
-print "V1 = %d, V2 = %d, V3 = %d\n" %(v1, v2, v3)
+print "V1 = %d, V2 = %d, V3 = %d\n" %(v1_correct, v2_correct, v3_correct)
 
 print "Вычисление объёма через умножение массива: "
 print v
+
+plt.xlabel(u'L, length')
+plt.ylabel(u'V, volume')
+plt.title('Volume depended on side length')  
+plt.plot(l, v)
