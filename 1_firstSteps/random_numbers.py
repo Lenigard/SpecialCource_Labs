cb6787f1c8b3e33dd687820e7119d1c76e339569
Lenigard <lenigard@mail.ru>
2016-11-10 12:40:43 +0300

    Change random to float values.

--- 1_firstSteps/random_numbers.py
+++ 1_firstSteps/random_numbers.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from random import randint as rand
+from random import uniform as rand
 
 size = 4
 numbers = []
 
 for i in range(0, size):
-    numbers.append(rand(0, 9))
+    numbers.append(rand(0, 10))
 
 print numbers
